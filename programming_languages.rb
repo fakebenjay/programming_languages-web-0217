34afaee0edf04c1c3e11f9f3bdc24c5144094677
Ben Jay <fakebenjay@gmail.com>
2017-01-28 23:54:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,67 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, info|
+    info.each do |language, type|
+      if new_hash[language] == nil
+        style_array = []
+        style_array << style
+        new_hash[language] = {type:type.values.join, style:style_array}
+      else
+        style_array = new_hash[language][:style]
+        style_array << style
+        new_hash[language] = {type:type.values.join, style:style_array}
+      end
+    end
+  end
+  return new_hash
 end
+
+###############################################################################
+#####Earlier version that doesn't work with languages with multiple styles#####
+###############################################################################
+
+#def reformat_languages(languages)
+#  new_hash = {}
+
+#  languages.each do |style, info|
+#    info.each do |language, type|
+#      style_array = []
+#      style_array << style
+#      new_hash[language] = {type:type.values.join, style:style_array}
+#    end
+#  end
+#  return new_hash
+#end
